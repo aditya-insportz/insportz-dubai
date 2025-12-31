633c5c8ccba54222e973116af2a6b05c21e11b11
Aditya Singh Rajput <153012701+indainboi@users.noreply.github.com>
2025-12-31 19:07:42 +0530

    added map in contact us

--- src/pages/ContactUs/index.tsx
+++ src/pages/ContactUs/index.tsx
@@ -10,9 +10,7 @@ function ContactUs() {
       <Navbar />
       <div className={styles["contact__wrapper"]}>
         <div className={styles["hero__section"]}>
-          <h1 className={styles["heading"]}>
-            Contact Us
-          </h1>
+          <h1 className={styles["heading"]}>Contact Us</h1>
         </div>
         <div className={styles["contact__us__content"]}>
           <div className={styles["content-item"]}>
@@ -21,6 +19,17 @@ function ContactUs() {
           <div className={styles["content-item"]}>
             <Form />
           </div>
+        </div>
+        <div className={styles["map__wrapper"]}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3423.850520752622!2d55.21063659853021!3d25.128270259722562!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6bc5433aa705%3A0x2d447f74e758f757!2sInsportz%20Club!5e0!3m2!1sen!2sin!4v1766300067335!5m2!1sen!2sin"
+            width="100%"
+            height="100%"
+            style={{ border: "0" }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </div>
       <Footer />
